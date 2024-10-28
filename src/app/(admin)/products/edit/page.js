4173e5b75547e1301643bfdbe95c5be1a8e8cb1a
OrxanMehdizade <mehdizadeorxan2000@gmail.com>
2024-10-28 16:38:15 +0400

    updated components

--- src/app/(admin)/products/edit/page.js
+++ src/app/(admin)/products/edit/page.js
@@ -1,28 +1,84 @@
-import TextArea from "@/components/TextArea";
-import Input from "@/components/Input";
+"use client";
+
+import CustomComponents from "@/components/CustomComponents";
 import Select from "@/components/Select";
+import { useEffect, useState } from "react";
 
-const EditProduct = async ({ searchParams }) => {
-  const productId = searchParams?.productId;
-  const response = await fetch(
-    `http://localhost:5000/products/product/${productId}`
-  );
-  const product = await response.json();
+const EditProduct = ({ searchParams }) => {
+  const [product, setProduct] = useState({});
 
+  const handleInputChange = (name, value) => {
+    setProduct((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const getProductData = async () => {
+    const productId = searchParams?.productId;
+    const response = await fetch(
+      `http://localhost:5000/products/product/${productId}`
+    );
+    const product = await response.json();
+    setProduct(product);
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, [searchParams]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="grid grid-cols-2 gap-4 h-fit p-[50px]">
-      <Input name="title" value={product.title} />
-
-      <TextArea name="description" value={product.description} />
-
-      <Input name="price" value={product.price} />
-
-      <Input name="stock" value={product.stock} />
-      <Select
-        options={["swdwdwdalam", "saqwdwdwol","wdwdwdwwdw"]}
-        value={product.category}
-        title="Categories"
+    <form
+      onSubmit={handleSubmit}
+      className="grid grid-cols-2 gap-4 h-fit p-[50px]"
+    >
+      <CustomComponents.Input
+        title="Title"
+        inputName="title"
+        inputValue={product.title}
+        handleInputChange={handleInputChange}
       />
+
+      <CustomComponents.Input
+        title="Description"
+        inputName="description"
+        inputValue={product.description}
+        handleInputChange={handleInputChange}
+      />
+
+      <CustomComponents.Input
+        title="Price"
+        inputName="price"
+        inputType="number"
+        inputValue={product.price}
+        handleInputChange={handleInputChange}
+      />
+
+      <CustomComponents.Input
+        title="Stock"
+        inputName="stock"
+        inputValue={product.stock}
+        handleInputChange={handleInputChange}
+      />
+      <CustomComponents.Select
+        title="Categories"
+        options={["swdwdwdalam", "saqwdwdwol", "wdwdwdwwdw"]}
+        inputName="category"
+        inputValue={product.category}
+        handleInputChange={handleInputChange}
+      />
+      <CustomComponents.Input
+        title="Gallery"
+        fileType=".jpg, .jepg, .png, .svg, .webp"
+        inputType="file"
+        inputName="gallery"
+        handleInputChange={handleInputChange}
+      />
+      <button
+        type="submit"
+        className="border-[1px] border-blue-800 bg-blue-500 text-white py-3 rounded-md"
+      >
+        Save
+      </button>
     </form>
   );
 };
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,9 +1,27 @@
 "use client";
 
 const Images = () => {
-    const addImage = async () => {
-        console.log("salam")
-    }
+  const addImage = async ({e}) => {
+    console.log(e)
+    // const response = await fetch("http://localhost:5000/products/addImage", {
+    //   method: "POST",
+    //   body: "",
+    //   headers: {
+    //     "Content-Type": "multiform/form-data",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
+  };
+  return (
+    <div>
+      {/* <form action={(e)=>{
+        addImage(e);
+      }}>
+        <input type="file" />
+        <button>Upload</button>
+      </form> */}
+    </div>
+  );
 };
 
 export default Images;
